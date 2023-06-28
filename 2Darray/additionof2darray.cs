c2e0755a6240ff5a01104f2b13e76fda247a8908
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-06-28 11:43:19 +0530

    update addition

---
+++ 2Darray/additionof2darray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Darray
+{
+    internal class additionof2darray
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Enter value for arr[{i},{j}]");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("-------------FirstMatrix----------------------");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------SecoundMatrix----------------------");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------Addition of two Matrix----------------------");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + arr[i,j]+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
